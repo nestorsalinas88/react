4ff3883c0578ead7fd7d9f9ce4d41c3056644916
nestor salinas <nestorasm88@gmail.com>
2018-11-12 16:52:58 -0600

    save

---
+++ sick-fits/frontend/pages/home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+
+const Casa = props => (
+    <div id='root'>
+        <div className={'slide1'}>
+            <p>I am trying</p>
+        </div>
+        <div className={'slide2'}>
+            <p>I am trying</p>
+        </div>
+        <div className={'slide3'}>
+         <p>I am trying</p>
+        </div>
+        <div className={'slide4'}>
+        <p>I am trying</p>
+        </div>
+        <div className={'slide5'}>
+        <p>I am trying</p>
+        </div>
+    </div>
+);
+
+export default Casa;
