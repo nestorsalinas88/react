83c08d3e9973f2940fabbade4881907b793456d0
nestor salinas <nestorasm88@gmail.com>
2018-12-05 13:03:36 -0600

    saving the end of chapter 14: setting up Apollo

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,8 +1,19 @@
- const Query = {
-    // dogs(parent, args, ctx, info) {
-    //     global.dogs = global.dogs || [];
-    //         return global.dogs;
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+
+    items: forwardTo('db'),
+
+    // async items(parent, args, ctx, info) {
+    //    const items = await ctx.db.query.items();
+    //     return items
     // },
- };
+};
+
+// dogs(parent, args, ctx, info) {
+//     global.dogs = global.dogs || [];
+//         return global.dogs;
+// },
+
 
 module.exports = Query;
--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -4,9 +4,8 @@ import Link from 'next/link';
 const Home = props => (
     <div>
         <p>Hey!</p>
-
-
     </div>
+
 
 
 );
